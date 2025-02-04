f0c1e6839b06a83ff9c207a1556d952cfee20859
balsem <balsem.khouni@esprit.tn>
2025-02-04 12:04:25 +0100

    seance 3 projet 1

--- src/main/java/com/ms/candidat/mscandidat/CandidateApplication.java
+++ src/main/java/com/ms/candidat/mscandidat/CandidateApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CandidateApplication {
     public static void main(String[] args) {
         SpringApplication.run(CandidateApplication.class, args);
--- src/main/java/com/ms/candidat/mscandidat/MScandidatApplication.java
+++ src/main/java/com/ms/candidat/mscandidat/MScandidatApplication.java
@@ -2,8 +2,10 @@ package com.ms.candidat.mscandidat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MScandidatApplication {
 
     public static void main(String[] args) {
